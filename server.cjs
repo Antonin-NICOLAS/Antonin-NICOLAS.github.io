f50673234fd04fb42e96fa5d76ed71ff4dbab16c
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-08 02:02:29 +0100

    Add files via upload

--- server.cjs
+++ server.cjs
@@ -3,8 +3,8 @@ const app = express()
 
 app.use(express.static('dist'))
 
-const port = process.env.VITE_APP_PORT || 8080
-const host = process.env.VITE_APP_HOST || 'localhost'
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost'
 
 app.listen(port, host, () => {
   console.log(`Server listening at http://${host}:${port}`)
