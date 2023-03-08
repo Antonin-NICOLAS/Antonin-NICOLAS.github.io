a6e1400b33f05393f5ca4b53f60927c1d526dab0
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-08 10:05:28 +0100

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -2,10 +2,4 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('dist'))
-
-const port = process.env.PORT || 3000
-const host = process.env.HOST || 'localhost'
-
-app.listen(port, host, () => {
-  console.log(`Server listening at http://${host}:${port}`)
-})
+app.listen(process.env.PORT || 3000)
