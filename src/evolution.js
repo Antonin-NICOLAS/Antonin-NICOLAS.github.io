be161bb3b1df5224082c97ed401917f7e9a6fac0
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-15 14:50:28 +0100

    Add files via upload

--- src/evolution.js
+++ src/evolution.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import * as THREE from 'three'
-import { OrbitControls } from './medias/glb/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 //ROBOT3D//
 
