084d310cf9daea4518a51cc0b7cad1637a0c04b0
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-23 21:51:36 +0100

    Delete copy-glb.cjs

--- src/copy-glb.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-fs.copy('./src/medias/glb', './dist/medias/glb', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Media files copied successfully!');
-  }
-});
