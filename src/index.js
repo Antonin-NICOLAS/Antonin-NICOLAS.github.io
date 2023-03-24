de3e98bc0644d7e12cb19f7d88f2ebe3ae930d1d
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-24 19:05:03 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import * as ScrollMagic from "scrollmagic";
 import { ScrollMagicPluginIndicator} from "scrollmagic-plugins";
-
-const parcelPath = new URL('../medias/glb/EARTH3D.glb', import.meta.url);
  
 
 //title animation//
@@ -58,7 +56,7 @@ gsap.to('.char', {
       // Chargement du modèle 3D
       const loader = new GLTFLoader();
       loader.load(
-        parcelPath.href , // chemin vers le modèle
+        '/medias/glb/EARTH3D.glb' , // chemin vers le modèle
         (gltf) => {
           scene.add(gltf.scene);
         },
