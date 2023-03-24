bb42ee8e7eb38ca6e0a7e1dc755751d9ad87096e
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-24 19:04:30 +0100

    Add files via upload

--- copy-glb.cjs
+++ copy-glb.cjs
@@ -1,9 +1,33 @@
 const fs = require('fs-extra');
 
-fs.copy('./src/medias/glb', './dist/medias/glb', (err) => {
+fs.mkdirSync('./dist/medias/glb', { recursive: true });
+
+
+fs.copyFile('./src/medias/glb/Robot.glb', './dist/medias/glb/Robot.glb', (err) => {
   if (err) {
     console.error(err);
   } else {
-    console.log('Media files copied successfully!');
+    console.log('Robot.glb file copied successfully!');
+  }
+});
+fs.copyFile('./src/medias/glb/EARTH3D.glb', './dist/medias/glb/EARTH3D.glb', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('EARTH3D.glb file copied successfully!');
+  }
+});
+fs.copyFile('./src/medias/glb/light-scene.splinecode', './dist/medias/glb/light-scene.splinecode', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Spline model file copied successfully!');
+  }
+});
+fs.copyFile('./src/medias/glb/ogimage.png', './dist/medias/glb/ogimage.png', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('OGimage png file copied successfully!');
   }
 });
