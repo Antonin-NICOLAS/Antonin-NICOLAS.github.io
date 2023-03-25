bdcbac784c6b835f29842630177d7079951378d4
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2023-03-25 10:48:14 +0100

    Add files via upload

--- src/evolution.js
+++ src/evolution.js
@@ -2,79 +2,86 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const glbUrl = 'https://storage.googleapis.com/robotglb/Robot.glb'
 //ROBOT3D//
 
-    // Initialisation de la scène
-    const scene = new THREE.Scene();
+// Initialisation de la scène
+const scene = new THREE.Scene();
 
-    const sizes = {
-        width: 300,
-        height: 150
-    }
-    // Initialisation de la caméra
-    const camera = new THREE.PerspectiveCamera(
-        75, // angle de vue
-        sizes.width / sizes.height, // ratio d'aspect
-        0.1, // distance de rendu minimale
-        1000 // distance de rendu maximale
-    );
+const sizes = {
+    width: 300,
+    height: 150
+}
+// Initialisation de la caméra
+const camera = new THREE.PerspectiveCamera(
+    75, // angle de vue
+    sizes.width / sizes.height, // ratio d'aspect
+    0.1, // distance de rendu minimale
+    1000 // distance de rendu maximale
+);
 
-    // Positionnement de la caméra et des lumières
-    camera.position.z = 0.35;
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-    scene.add(ambientLight);
+// Positionnement de la caméra et des lumières
+camera.position.z = 0.35;
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambientLight);
 
-    const dirLight = new THREE.DirectionalLight(0xefefff, 1.5);
-    dirLight.position.set(10, 10, 10);
-    scene.add(dirLight);
+const dirLight = new THREE.DirectionalLight(0xefefff, 1.5);
+dirLight.position.set(10, 10, 10);
+scene.add(dirLight);
 
-    // Initialisation du rendu
-    const canvas = document.querySelector('.ROBOT3D');
-    const renderer = new THREE.WebGLRenderer({
-        canvas: canvas,
-        antialias: true,
-        alpha: true
+// Initialisation du rendu
+const canvas = document.querySelector('.ROBOT3D');
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+    alpha: true
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Initialisation des contrôles OrbitControls
+const controls = new OrbitControls(camera, canvas);
+controls.autoRotate = true;
+controls.enableZoom = false;
+controls.enableDamping = true;
+
+// Chargement du modèle 3D
+const loader = new GLTFLoader();
+fetch('https://storage.googleapis.com/robotglb/Robot.glb')
+    .then(response => response.blob())
+    .then(blob => {
+        // Utilisez createObjectURL pour créer une URL blob à partir de la réponse HTTP
+        const blobURL = URL.createObjectURL(blob);
+
+        // Chargez le fichier GLB à l'aide du loader GLTF de Three.js
+        loader.load(
+            blobURL,
+            (gltf) => {
+                scene.add(gltf.scene);
+            },
+            undefined,
+            (error) => {
+                console.error(error);
+            }
+        );
     });
-    renderer.setSize(sizes.width, sizes.height);
 
-    // Initialisation des contrôles OrbitControls
-    const controls = new OrbitControls(camera, canvas);
-    controls.autoRotate = true;
-    controls.enableZoom = false;
-    controls.enableDamping = true;
+// Fonction d'animation
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+animate();
 
-    // Chargement du modèle 3D
-    const loader = new GLTFLoader();
-    loader.load(
-        glbUrl, // Ajoutez l'url du fichier glb après l'adresse du serveur proxy
-        (gltf) => {
-            scene.add(gltf.scene);
-        },
-        undefined,
-        (error) => {
-            console.error(error);
-        }
-    );
-
-    // Fonction d'animation
-    function animate() {
-        requestAnimationFrame(animate);
-        controls.update();
-        renderer.render(scene, camera);
-    }
-    animate();
-
-    //responsive
-    window.addEventListener('resize', () => {
-        //update sizes
-        sizes.width = 300,
+//responsive
+window.addEventListener('resize', () => {
+    //update sizes
+    sizes.width = 300,
         sizes.height = 150
-        //update camera
-        camera.aspect = sizes.width / sizes.height
-        camera.updateProjectionMatrix()
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-        //update renderer
-        renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    })
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
